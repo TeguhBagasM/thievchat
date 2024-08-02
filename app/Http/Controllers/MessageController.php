817a1717c7f265342b8dead352ea740124699fb9
Teguh Bagas Mardiansyah <teguhbagas2134@gmail.com>
2024-08-02 09:36:46 +0700

    create  method

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -29,17 +29,41 @@ class MessageController extends Controller
 
     public function byGroup(Group $group)
     {
+        $messages = Message::where('group_id', $group->id)
+            ->latest()
+            ->paginate(50);
 
+        return Inertia('Home', [
+            'selectedConversation' => $group->toConversationArray(),
+            'messages' => MessageResource::collection($messages),
+        ]);
     }
 
     public function loadOlder(Message $message)
     {
+        if ($message->group_id) {
+            $$messages = Message::where('created_at', '<', $message->created_at)
+                ->where('group_id', $message->group_id)
+                ->latest()
+                ->paginate(10);
+        } else {
+            $messages = Message::where('created_at', '<', $message->created_at)
+                ->where(function ($query) use ($message) {
+                    $query->where('sender_id', $message->sender_id)
+                        ->where('receiver_id', $message->receiver_id)
+                        ->orWhere('sender_id', $message->receiver_id)
+                        ->where('receiver_id', $message->sender_id);
+                    })
+                    ->latest()
+                    ->paginate(10);
+        }
 
+        return MessageResource::collection($messages);
     }
 
     public function store(StoreMessageRequest $request)
     {
-
+        
     }
 
     public function destroy(Message $message)
