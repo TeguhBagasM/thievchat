4e6c28699c0538dc7258bb2f5b2c6314574608c7
Teguh Bagas Mardiansyah <teguhbagas2134@gmail.com>
2024-08-02 14:12:32 +0700

    render messages

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,13 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\SocketMessage;
 use App\Http\Requests\StoreMessageRequest;
 use App\Http\Resources\MessageResource;
+use App\Models\Conversation;
 use App\Models\Group;
 use App\Models\Message;
+use App\Models\MessageAttachment;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
+use Illuminate\Support\Str;
 
 class MessageController extends Controller
 {
@@ -31,7 +36,7 @@ class MessageController extends Controller
     {
         $messages = Message::where('group_id', $group->id)
             ->latest()
-            ->paginate(50);
+            ->paginate(10);
 
         return Inertia('Home', [
             'selectedConversation' => $group->toConversationArray(),
@@ -42,7 +47,7 @@ class MessageController extends Controller
     public function loadOlder(Message $message)
     {
         if ($message->group_id) {
-            $$messages = Message::where('created_at', '<', $message->created_at)
+            $messages = Message::where('created_at', '<', $message->created_at)
                 ->where('group_id', $message->group_id)
                 ->latest()
                 ->paginate(10);
@@ -63,11 +68,55 @@ class MessageController extends Controller
 
     public function store(StoreMessageRequest $request)
     {
-        
+        $data = $request->validated();
+
+        $data['sender_id'] = auth()->id();
+        $receiverId = $data['receiver_id'] ?? null;
+        $groupId = $data['group_id'] ?? null;
+
+        $files = $data['attachments'] ?? [];
+        $message = Message::create($data);
+
+        $attachments = [];
+        if ($files) {
+            foreach ($files as $file) {
+                $directory = 'attachments/' . Str::random(32);
+                Storage::makeDirectory($directory);
+
+                $model = [
+                    'message_id' => $message->id,
+                    'name' => $file->getClientOriginalName(),
+                    'mime' => $file->getClientMimeType(),
+                    'size' => $file->getSize(),
+                    'path' => $file->store($directory, 'public'),
+                ];
+
+                $attachment = MessageAttachment::create($model);
+                $attachments[] = $attachment;
+            }
+            $message->attachments = $attachments;
+        }
+
+        if ($receiverId) {
+            Conversation::updateConversationWithMessage($receiverId, auth()->id(), $message);
+        }
+
+        if ($groupId) {
+            Group::updateGroupWithMessage($groupId, $message);
+        }
+        SocketMessage::dispatch($message);
+
+        return new MessageResource($message);
     }
 
     public function destroy(Message $message)
     {
+        if ($message->sender_id !== auth()->id()) {
+            return response()->json(['message' => 'You can only delete your own messages.'], 403);
+        }
 
+        $message->delete();
+
+        return response()->json('', 204);
     }
 }
--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,8 +1,59 @@
 import ChatLayout from "@/Layouts/ChatLayout";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { ChatBubbleLeftRightIcon } from "@heroicons/react/24/solid";
+import { useEffect, useRef, useState } from "react";
 
-function Home({ auth }) {
-    return <>Messages</>;
+function Home({ messages }) {
+    const [localMessages, setLocalMessages] = useState([]);
+    const messagesCtrRef = useRef(null);
+
+    useEffect(() => {
+        setLocalMessages(messages);
+    }, [messages]);
+    return (
+        <>
+            {!messages && (
+                <div className="flex flex-col gap-8 justify-center items-center text-center h-full opacity-35">
+                    <div className="text-2xl md:text-4xl p-16 text-slate-200">
+                        Please select conversation to start messaging
+                    </div>
+                    <ChatBubbleLeftRightIcon className="w-32 h-32 inline-block" />
+                </div>
+            )}
+
+            {messages && (
+                <>
+                    <ConversationHeader
+                        selectedConversation={selectedConversation}
+                    />
+                    <div
+                        ref={messagesCtrRef}
+                        className="flex-1 overflow-y-auto p-5"
+                    >
+                        {localMessages.length > 0 && (
+                            <div className="flex justify-center items-center h-full">
+                                <div className="text-lg text-slate-200">
+                                    No messages found
+                                </div>
+                            </div>
+                        )}
+
+                        {localMessages.length > 0 && (
+                            <div className="flex-1 flex flex-col">
+                                {localMessages.map((message) => (
+                                    <MessageItem
+                                        key={message.id}
+                                        message={message}
+                                    />
+                                ))}
+                            </div>
+                        )}
+                    </div>
+                    <MessageInput conversation={selectedConversation} />
+                </>
+            )}
+        </>
+    );
 }
 
 Home.layout = (page) => {
