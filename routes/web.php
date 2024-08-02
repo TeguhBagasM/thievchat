cc79fe5748f8f03dc8d22e0e2747d9c5dcadc350
Teguh Bagas Mardiansyah <teguhbagas2134@gmail.com>
2024-08-02 08:50:50 +0700

    create chatlayout and render conversations

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,13 @@ use Inertia\Inertia;
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/', [HomeController::class, 'home'])->name('dashboard');
+
+    Route::get('/user/user', function () {
+        
+    })->name('chat.user');
+    Route::get('/group/group', function () {
+        
+    })->name('chat.group');
 });
 
 Route::middleware('auth')->group(function () {
