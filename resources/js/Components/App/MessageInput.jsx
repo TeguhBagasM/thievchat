36a636a50320d20ae34f95b46b38c717739a91be
Teguh Bagas Mardiansyah <teguhbagas2134@gmail.com>
2024-08-05 06:14:37 +0700

    sending and downloading attachments

--- resources/js/Components/App/MessageInput.jsx
+++ resources/js/Components/App/MessageInput.jsx
@@ -4,6 +4,7 @@ import {
     PaperAirplaneIcon,
     PaperClipIcon,
     PhotoIcon,
+    XCircleIcon,
 } from "@heroicons/react/24/solid";
 
 import { useState } from "react";
@@ -16,6 +17,22 @@ const MessageInput = ({ conversation = null }) => {
     const [newMessage, setNewMessage] = useState("");
     const [inputErrorMessage, setInputErrorMessage] = useState("");
     const [messageSending, setMessageSending] = useState(false);
+    const [chosenFiles, setChosenFiles] = useState([]);
+    const [uploadProgress, setUploadProgress] = useState(0);
+
+    const onFileChange = (ev) => {
+        const files = ev.target.files;
+        const updatedFiles = [...files].map((file) => {
+            return {
+                file: file,
+                url: URL.createObjectURL(file),
+            };
+        });
+
+        setChosenFiles((prevFiles) => {
+            return [...prevFiles, ...updatedFiles];
+        });
+    };
 
     const onSendClick = () => {
         if (messageSending) {
@@ -32,6 +49,9 @@ const MessageInput = ({ conversation = null }) => {
             return;
         }
         const formData = new FormData();
+        chosenFiles.forEach((file) => {
+            formData.append("attachments[]", file.file);
+        });
         formData.append("message", newMessage);
 
         if (conversation.is_user) {
@@ -49,14 +69,21 @@ const MessageInput = ({ conversation = null }) => {
                     );
 
                     console.log(progress);
+                    setUploadProgress(progress);
                 },
             })
             .then((response) => {
                 setNewMessage("");
                 setMessageSending(false);
+                setUploadProgress(0);
+                setChosenFiles([]);
             })
             .catch((error) => {
                 setMessageSending(false);
+                setChosenFiles([]);
+                const message = error?.response?.data?.message(
+                    message || "An error occurred while sending the message"
+                );
             });
     };
 
@@ -84,6 +111,7 @@ const MessageInput = ({ conversation = null }) => {
                     <input
                         type="file"
                         multiple
+                        onChange={onFileChange}
                         className="absolute left-0 top-0 right-0 bottom-0 z-20 opacity-0 cursor-pointer"
                     />
                 </button>
@@ -94,6 +122,7 @@ const MessageInput = ({ conversation = null }) => {
                         type="file"
                         multiple
                         accept="image/*"
+                        onChange={onFileChange}
                         className="absolute left-0 top-0 right-0 bottom-0 z-20 opacity-0 cursor-pointer"
                     />
                 </button>
@@ -118,10 +147,59 @@ const MessageInput = ({ conversation = null }) => {
                         <PaperAirplaneIcon className="w-6" />
                         {/* <span className="hidden sm:inline">Send</span> */}
                     </button>
-                </div>
+                </div>{" "}
+                {!uploadProgress && (
+                    <progress
+                        className="progress progress-info w-full"
+                        value={uploadProgress}
+                        max="100"
+                    ></progress>
+                )}
                 {inputErrorMessage && (
                     <p className="text-xs text-red-400">{inputErrorMessage}</p>
                 )}
+                <div className="flex flex-wrap gap-1 mt-2">
+                    {chosenFiles.map((file) => (
+                        <div
+                            key={file.file.name}
+                            className={
+                                `relative flex justify-between cursor-pointer ` +
+                                (!isImage(file.file) ? "w-[240px]" : "")
+                            }
+                        >
+                            {isImage(file.file) && (
+                                <img
+                                    src={file.url}
+                                    alt=""
+                                    className="w-16 h-16 object-cover"
+                                />
+                            )}
+                            {isAudio(file.file) && (
+                                <CustomAudioPlayer
+                                    file={file}
+                                    showVolume={false}
+                                />
+                            )}
+                            {isAudio(file.file) && isImage(file.file) && (
+                                <AttachmentPreview file={file} />
+                            )}
+
+                            <button
+                                onClick={() =>
+                                    setChosenFiles(
+                                        chosenFiles.filter(
+                                            (f) =>
+                                                f.file.name !== file.file.name
+                                        )
+                                    )
+                                }
+                                className="absolute w-6 h-6 rounded-full bg-gray-800 -right-2 -top-2 text-gray-300 hover:text-gray-100 z-10"
+                            >
+                                <XCircleIcon className="w-6" />
+                            </button>
+                        </div>
+                    ))}
+                </div>
             </div>
             <div className="order-3 xs:order-3 p-2 flex">
                 <Popover className="relative">
